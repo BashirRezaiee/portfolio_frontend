9e1c37fcda2459330c09d8ddddfc45a0d5e6f9d3
Abdul Bashir Rezaie <abdulbashirrezaie@gmail.com>
2022-12-12 23:03:54 +0430

    Create App Wrapper

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
 import './About.scss'
 import { client, urlFor } from '../../client'
+import { AppWrap } from '../../wrapper'
 
 const About = () => {
   const [abouts, setAbouts] = useState(null)
@@ -36,4 +37,4 @@ const About = () => {
   )
 }
 
-export default About
+export default AppWrap(About, 'about');
--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion'
 import { images } from '../../constants'
 
 import './Header.scss'
+import { AppWrap } from '../../wrapper'
 
 const Header = () => {
   const scaleVariants = {
@@ -17,7 +18,7 @@ const Header = () => {
   }
 
   return (
-    <div id='home' className='app__header app__flex'>
+    <div className='app__header app__flex'>
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
@@ -71,4 +72,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default AppWrap(Header, 'home');
